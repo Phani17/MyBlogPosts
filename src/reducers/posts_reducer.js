63f895d210fd993e1bb18913620c4e093614761a
Phani17 <phanindrasarma20@gmail.com>
2017-08-21 09:12:08 +0530

    Update posts_reducer.js

--- src/reducers/posts_reducer.js
+++ src/reducers/posts_reducer.js
@@ -1,9 +1,6 @@
 import { FETCH_POSTS, FETCH_POST } from '../actions/index'
 const INITIAL_STATE ={ posts:[], post:null }
 
-//from reducer we need to have
-//1. data from axios is avalable at action.payload.data
-//2. reducer need to return a new object whenever we need to return our state
 export default function(state=INITIAL_STATE,action){
   switch (action.type) {
     case FETCH_POSTS:
